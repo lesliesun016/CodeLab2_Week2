1131394ff07ad772a4cbfefab55985d1d0d38299
lesliesun016 <leslieyiyuanzeng@gmail.com>
2022-09-09 15:23:23 -0400

    edited comments

--- Assets/Scripts/RepopulateScript.cs
+++ Assets/Scripts/RepopulateScript.cs
@@ -10,10 +10,12 @@ public class RepopulateScript : MonoBehaviour {
 		gameManager = GetComponent<GameManagerScript>();
 	}
 
-	// fill in empty tokens through the game manager
+	// fill empty tokens in the grid
 	public virtual void AddNewTokensToRepopulateGrid(){
+		// go through every token in the grid
 		for(int x = 0; x < gameManager.gridWidth; x++){
 			GameObject token = gameManager.gridArray[x, gameManager.gridHeight - 1];
+			// if token is empty, add new tokens to the grid
 			if(token == null){
 				gameManager.AddTokenToPosInGrid(x, gameManager.gridHeight - 1, gameManager.grid);
 			}
